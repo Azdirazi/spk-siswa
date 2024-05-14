b007710e22d37eb466ef3b8a25218b2a5208ffd4
Azdirazi <91419014+Azdirazi@users.noreply.github.com>
2024-05-14 17:01:05 +0700

    tabel

--- database/migrations/2024_05_13_144840_create_criteria_table.php
+++ database/migrations/2024_05_13_144840_create_criteria_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('criteria', function (Blueprint $table) {
             $table->id();
+            $table->string('code');
+            $table->string('weight');
             $table->string('name');
-            $table->int('role');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->smallInteger('attribute');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('criteria');
     }
 };
--- database/migrations/2024_05_13_145155_create_sub_criteria_table.php
+++ database/migrations/2024_05_13_145155_create_sub_criteria_table.php
@@ -11,14 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sub_criteria', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('criteria_id')
+                ->references('id')
+                ->on('criteria')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->string('code');
             $table->string('name');
-            $table->int('role');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('value');
             $table->timestamps();
         });
     }
@@ -28,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sub_criteria');
     }
 };
